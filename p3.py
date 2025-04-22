36787fa99b7e629275e8c2a55e1d13dc958168e3
Yogesh-Bhoye <bhoyey840@gmail.com>
2025-04-22 23:48:13 +0530

    p3.py

--- p3.py
+++ p3.py
@@ -1,9 +1,2 @@
-# Initialize a list
-my_list = [1, 2, 3, 4, 5]
-
-# Interchange first and last elements
-my_list[0], my_list[-1] = my_list[-1], my_list[0]
-
-# Print the modified list
-print("List after swapping first and last elements:", my_list)
+print("hello")
 
